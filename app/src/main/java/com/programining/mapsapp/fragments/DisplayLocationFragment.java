ad39ee45c6281bdbde08716653ce031e7e69301c
Yousuf <josephali1993@gmail.com>
2020-05-03 17:23:36 +0400

    commit

--- app/src/main/java/com/programining/mapsapp/fragments/DisplayLocationFragment.java
+++ app/src/main/java/com/programining/mapsapp/fragments/DisplayLocationFragment.java
@@ -4,16 +4,14 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
